db5196b0ae88ab242a112385c5bbdb1966fe05b9
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2019-01-11 13:40:30 +0200

    site

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    $('.lower-body__table td a').click(function(e) {
+        e.preventDefault();
+        $('.popup-outer').fadeIn('slow');
+
+        
+
+    });
+
+    $('.button').click(function() {
+        $('.popup-outer').fadeOut('slow');  
+    });
+
+
+    $(".link1").click(function() {
+        $('.content').text($('.date1').text());
+      });
+
+      $(".link2").click(function() {
+        $('.content').text($('.date2').text());
+      });
+
+      $(".link3").click(function() {
+        $('.content').text($('.date3').text());
+      });
+
+      $(".link4").click(function() {
+        $('.content').text($('.date4').text());
+      });
+
+      $(".link5").click(function() {
+        $('.content').text($('.date5').text());
+      });
+
+      $(".link6").click(function() {
+        $('.content').text($('.date7').text());
+      });
+
+      $(".link8").click(function() {
+        $('.content').text($('.date9').text());
+      });
+
+      $(".link10").click(function() {
+        $('.content').text($('.date10').text());
+      });
+
+      $(".link11").click(function() {
+        $('.content').text($('.date11').text());
+      });
+
+      $(".link12").click(function() {
+        $('.content').text($('.date12').text());
+      });
+
+      $(".link13").click(function() {
+        $('.content').text($('.date13').text());
+      });
+
+      $(".link14").click(function() {
+        $('.content').text($('.date14').text());
+      });
+
+
+});
+
